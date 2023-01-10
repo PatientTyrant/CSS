0ea76e423504ca4a596abf6ff523cfefe4b17c23
PatientTyrant <105866700+PatientTyrant@users.noreply.github.com>
2023-01-10 21:47:03 +0300

    Index,internal/externalselect,stylecss

--- CSS/Program.cs
+++ CSS/Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.UseStaticFiles();   // wwwroot klasörü içerisindeki static dosyalarý eriþime açar
+
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
